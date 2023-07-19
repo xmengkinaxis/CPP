ca77ebc55e1a982582be167e0309c6ff5cb081ff
David Meng <xiangliang.david.meng@gmail.com>
2023-07-18 23:13:02 -0400

    Update DFS and BFS, and remove the duplicate

--- scratch.cpp
+++ scratch.cpp
@@ -44,11 +44,15 @@ int fnTwoPointersTwoArrays(vector<int>& arr1, vector<int>& arr2) {
 
 
 // sliding window
+// for the window with the various size, need two pointers to indicate the start and the end of the window
+// for the window with the fixed size, only need a single pointer to point to the end of the window
+// for the window with the fixed size, remove the start since the end >= the window size
+// must ensure the start is moved, otherwise, the inner loop would become a dead one.
 int fnSlidingWindow(vector<int>& arr) {
 	int ans = 0; 
 	for (int left = 0, right = 0, curr = 0; right < arr.size(); ++right) {
 		// add arr[right] into curr; 
-		while (CoNDITION && left <= right ) {
+		while (CONDITION && left <= right ) {
 			// remove arr[left] from curr; 
 			++left; 
 		}
@@ -121,6 +125,7 @@ int fnMonotonicIncreasingStack(vector<int>& arr) {
 	}
 }
 
+// inorder, preorder, postorder;
 
 int dfsTree(TreeNode *root) {
 	// 1. deal withing the special base cases
@@ -130,6 +135,8 @@ int dfsTree(TreeNode *root) {
 	int ans = 0; 
 	// 2. do logic for this node
 	// 3. iterate all branches/candidates/choices/
+	// 4. might need to check if a branch should be visited with some conditions
+	//  or depending on if other branches exist or not
 	dfsTree(root->left); 
 	dfsTree(root->right); 
 	return ans; 	
@@ -142,6 +149,7 @@ int dfsTreeStack(TreeNode* root) {
 	stack<TreeNode*> stack; 
 	stack.push(root); 
 	int ans = 0; 
+	// beside if the stack is empty, it might need to check if other conditions are met
 	while (!stack.empty()) {
 		auto node = stack.top(); 
 		stack.pop(); 
@@ -163,24 +171,41 @@ int bfsTree(TreeNode* root) {
 	queue<TreeNode*> queue; 
 	queue.push(root); 
 	int ans = 0; 
+	// beside if the queue is empty, it might need to check if other conditions are met
 	while (!queue.empty()) {
+		// the following for loop can be eliminated if there is NO action for each layer
 		for (int count = queue.size(); 0 < count; --count) {
 			auto node = queue.front(); queue.pop(); 
 			// do logic for node
 			if (node->left) { queue.push(node->left); }
 			if (node->right) { queue.push(node->right); }
 		}
+		// do some actions for this layer, e.g. calculating steps/time
+		// or this action can be merged with the while loop and change it into a for loop
 	}
 	return ans; 
 }
 
+// for DFS, the core actions are: 
+// 1. check if the node is valid, e.g. it must be in the grid when the problem is a grid, 
+// 2. check if the node is already visited, e.g. seen/visited already contains it
+// 3. check if the node should be visited, e.g. the original value/attribute of this node makes it as a valid candidate. 
+// 4. do something for this node; this is a real visit on this node
+// 5. visit all its neighbors; 
+// * these core actions is the core of DFS and the block of the BFS while loop
+// * for BFS, the order might be 4, 5, 1, 2, 3
+// * if changing the original value instead of using seen/visited, the 2 and 3 are merged into one of checking value
+
+
 unordered_set<int> seen; 
-int dfsGraph(vector<vector<int>>& graph) {
-	seen.insert(START_NODE); // must do insert before visiting it in order to avoid the dead loop
+int dfsGraph1(vector<vector<int>>& graph) {
+	seen.insert(START_NODE); // #1. must do insert before visiting it in order to avoid the dead loop
+							// #2. alternatively, immediately insert it when start visiting it, 
+							// and check if the neighbor is already visited when iterating them 
 	return dfsGraph(START_NODE, graph);
 }
 
-int dfsGraph(int node, vector<vector<int>>& graph) {
+int dfsGraph1(int node, vector<vector<int>>& graph) {
 	int ans = 0; 
 	// do logic for node here
 	for (int neighbor : graph[node]) { // might to validate if a neighbor is valid first		
@@ -188,8 +213,32 @@ int dfsGraph(int node, vector<vector<int>>& graph) {
 			// they are a pair of actions, inserting the node into seen and call dfs on this node.
 			// an atomic action: add into seen, and then dfs ths node
 			// AKA. the atomic action: want to visit (add into seen), and do visit (do dfs)
-			seen.insert(neighbor); // 
+			seen.insert(neighbor); // this and the next steps are repeating the two steps in the above function
 			dfsGraph(neighbor, graph); 
+		}
+	}
+	return ans; 
+}
+
+
+// ????
+unordered_set<int> seen2; 
+int dfsGraph2(vector<vector<int>>& graph) {
+	return dfsGraph(START_NODE, graph);
+}
+
+int dfsGraph2(int node, vector<vector<int>>& graph) {
+	seen2.insert(START_NODE); // must do insert before visiting it in order to avoid the dead loop
+	int ans = 0; 
+	// do logic for node here
+	// if the node value can be changed and the changed value can be used as the visited flag, 
+	// then there is no need to create a separated seen/visited data structure
+	for (int neighbor : graph[node]) { // might to validate if a neighbor is valid first		
+		if (seen2.find(neighbor) = seen2.end()) { // prerequisite of the atomic action: ensure the node is not visited before
+			// they are a pair of actions, inserting the node into seen and call dfs on this node.
+			// an atomic action: add into seen, and then dfs ths node
+			// AKA. the atomic action: want to visit (add into seen), and do visit (do dfs)
+			dfsGraph2(neighbor, graph); 
 		}
 	}
 	return ans; 
@@ -214,19 +263,24 @@ int dfsIterative(vector<vector<int>> & graph) {
 	return ans; 
 }
 
+// when do Topological Sort, no need to keep the seen set, 
+// for it is DAG, and a node is added into queue only when only its in/out degree reach zero
 
 int bfsGraph(vector<vector<int>>& graph) {
 	queue<int> queue; 
 	unordered_set<int> seen; 
 	// they are a pair of actions, inserting the node into seen and adding the node into queen.
 	// an atomic action: add into seen, and then add into queue
+	// there might be a numbers of starting nodes and all these need to be added into the queue initially
+	// there might be zero of starting nodes. This is an edge case. 
+	// in summary, the number of starting nodes, could be 0, 1, N; 
 	seen.insert(start_node); 
 	queue.push(start_node); 
 	int ans = 0; 
 	while (!queue.empty()) {
 		int node = queue.front(); 
 		queue.pop(); 
-		// do logic for the node; 
+		// do logic for the node; e.g. set the distance or time for this node, or check if reach the destination and then exit
 		for (auto neighbor : graph[node]) {
 			// iterate candidates or validate if it is a valid candidate
 			if (seen.find(neighbor) == seen.end()) {
@@ -241,18 +295,18 @@ int bfsGraph(vector<vector<int>>& graph) {
 }
 
 vector<int> fnTopK(vector<int>& arr, int k) {
-	priority_queue<int> heap; 
+	priority_queue<int, vector<int>, greater<int>> minHeap; // priority_queue<int> heap; 
 	for (auto n : arr) {
-		heap.push(n); 
-		if (heap.size() > k) {
-			heap.pop();
+		minHeap.push(n); 
+		if (minHeap.size() > k) {
+			minHeap.pop();
 		}
 	}
 
 	vector<int> ans; 
-	while (heap.size() > 0){
-		ans.push_back(heap.top()); 
-		heap.pop(); 
+	while (minHeap.size() > 0){
+		ans.push_back(minHeap.top()); 
+		minHeap.pop(); 
 	}
 	return ans; 
 }
@@ -387,28 +441,6 @@ TrieNode* buildTrie(vector<string> words) {
 		}
 	}
 }
-
-struct TrieNode {
-	int data; 
-	unordered_map<char, TrieNode*> children; 
-	TrieNode() : data(0), children(unordered_map<char, TrieNode*>()) {}
-};
-
-TrieNode* buildTrie(vector<string> words) {
-	TrieNode* root = new TrieNode; 
-	for (auto word : words) {
-		auto curr = root; 
-		for (auto c : word) {
-			if (curr->children.find(c) == curr->children.end()) {
-				curr->children[c] = new TrieNode; 
-			}
-			curr = curr->children[c];
-			// do something here
-		}
-	}
-	return root; 
-}
-
 
 // Input size VS time complexity
 n <= 10 : O(n^2 * n!) or O(4^n), backtrack or brute-force 
